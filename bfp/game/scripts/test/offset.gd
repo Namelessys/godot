e75b31c17c68162b536df882adfc7308b1f2e1f6
MisterNoNameLP <misternonamelp@web.de>
2023-09-04 10:03:41 +0200

    Changes

--- bfp/game/scripts/test/offset.gd
+++ bfp/game/scripts/test/offset.gd
@@ -1,5 +1,6 @@
-extends Node
+extends Node2D
 
+@onready var area = $ColorRect/Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	print(get_transform())
+	print(area.get_transform())
 	pass
